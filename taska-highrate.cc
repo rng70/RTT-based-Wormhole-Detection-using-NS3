2fc2fe1cb8157096303e762fa3f6a4fa1077bcfb
rng70 <1705070@ugrad.cse.buet.ac.bd>
2022-02-18 22:09:21 +0600

    task a almost complete

--- taska-highrate.cc
+++ taska-highrate.cc
@@ -1,3 +1,19 @@
+#include "ns3/command-line.h"
+#include "ns3/config.h"
+#include "ns3/string.h"
+#include "ns3/log.h"
+#include "ns3/yans-wifi-helper.h"
+#include "ns3/ssid.h"
+#include "ns3/mobility-helper.h"
+#include "ns3/on-off-helper.h"
+#include "ns3/yans-wifi-channel.h"
+#include "ns3/mobility-model.h"
+#include "ns3/packet-sink.h"
+#include "ns3/packet-sink-helper.h"
+#include "ns3/tcp-westwood.h"
+#include "ns3/internet-stack-helper.h"
+#include "ns3/ipv4-address-helper.h"
+#include "ns3/ipv4-global-routing-helper.h"
 #include "ns3/core-module.h"
 #include "ns3/point-to-point-module.h"
 #include "ns3/network-module.h"
@@ -7,6 +23,9 @@
 #include "ns3/internet-module.h"
 #include "ns3/yans-wifi-helper.h"
 #include "ns3/ssid.h"
+#include "ns3/flow-monitor-helper.h"
+#include "ns3/flow-monitor-module.h"
+#include "ns3/traffic-control-module.h"
 
 // Default Network Topology
 //
@@ -22,39 +41,231 @@
 
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE("ThirdScriptExample");
+NS_LOG_COMPONENT_DEFINE("TaskAHighRate");
 
-int main(int argc, char* agrv[]){
-    bool verbose = true;
+std::ofstream throughput("./highrateSimulation/throughput.txt");
+Ptr<PacketSink> sink;     /* Pointer to the packet sink application */
+uint64_t lastTotalRx = 0; /* The value of the last total received bytes */
+
+void CalculateThroughput()
+{
+    Time now = Simulator::Now();                                       /* Return the simulator's virtual time. */
+    double cur = (sink->GetTotalRx() - lastTotalRx) * (double)8 / 1e5; /* Convert Application RX Packets to MBits. */
+    std::cout << now.GetSeconds() << "s: \t" << cur << " Mbit/s" << std::endl;
+    throughput << now.GetSeconds() << "s: \t" << cur << " Mbit/s" << std::endl;
+    lastTotalRx = sink->GetTotalRx();
+    Simulator::Schedule(MilliSeconds(100), &CalculateThroughput);
+}
+
+int main(int argc, char *argv[])
+{
+    // bool verbose = true;
     uint32_t nCsma = 3;
     uint32_t nWifi = 3;
-    bool tracing = false;
+    int no_of_flow = 3;
+    // bool pcapTracing = false; // TODO delete
 
-
+    uint32_t payloadSize = 1472;           /* Transport layer payload size in bytes. */
+    std::string dataRate = "100Mbps";      /* Application layer datarate. */
+    std::string tcpVariant = "TcpNewReno"; /* TCP variant type. */
+    std::string phyRate = "HtMcs7";        /* Physical layer bitrate. */
+    double simulationTime = 10;            /* Simulation time in seconds. */
 
     CommandLine cmd(__FILE__);
     cmd.AddValue("nCsma", "Number of \"extra\" CSMA nodes/devices", nCsma);
     cmd.AddValue("nWifi", "Number of wifi STA devices", nWifi);
-    cmd.AddValue("verbose", "Tell echo applications to log if true", verbose);
-    cmd.AddValue("tracing", "Enable pcap tracing", tracing);
+    cmd.AddValue("no_of_flow", "Number of flow", no_of_flow);
+    cmd.AddValue("payloadSize", "Payload size in bytes", payloadSize);
+    cmd.AddValue("dataRate", "Application data ate", dataRate);
+    cmd.AddValue("tcpVariant", "Transport protocol to use: TcpNewReno, "
+                               "TcpHybla, TcpHighSpeed, TcpHtcp, TcpVegas, TcpScalable, TcpVeno, "
+                               "TcpBic, TcpYeah, TcpIllinois, TcpWestwood, TcpWestwoodPlus, TcpLedbat ",
+                 tcpVariant);
+    cmd.AddValue("phyRate", "Physical layer bitrate", phyRate);
+    // cmd.AddValue("pcap", "Enable/disable PCAP tracing", pcapTracing); // TODO delete
+    cmd.AddValue("simulationTime", "Simulation time in seconds", simulationTime);
+    cmd.Parse(argc, argv);
 
-    cmd.Parse(argc, agrv);
-
-    // The underlying restriction of 18 is due to the grid position
-    // allocator's configuration; the grid layout will exceed the
-    // bounding box if more than 18 nodes are provided.
-
-    if (nWifi > 18)
+    tcpVariant = std::string("ns3::") + tcpVariant;
+    // Select TCP variant
+    if (tcpVariant.compare("ns3::TcpWestwoodPlus") == 0)
     {
-        std::cout << "nWifi should be 18 or less; otherwise grid layout exceeds the bounding box" << std::endl;
-        return 1;
+        // TcpWestwoodPlus is not an actual TypeId name; we need TcpWestwood here
+        Config::SetDefault("ns3::TcpL4Protocol::SocketType", TypeIdValue(TcpWestwood::GetTypeId()));
+        // the default protocol type in ns3::TcpWestwood is WESTWOOD
+        Config::SetDefault("ns3::TcpWestwood::ProtocolType", EnumValue(TcpWestwood::WESTWOODPLUS));
+    }
+    else
+    {
+        TypeId tcpTid;
+        NS_ABORT_MSG_UNLESS(TypeId::LookupByNameFailSafe(tcpVariant, &tcpTid), "TypeId " << tcpVariant << " not found");
+        Config::SetDefault("ns3::TcpL4Protocol::SocketType", TypeIdValue(TypeId::LookupByName(tcpVariant)));
     }
 
-    if (verbose)
+    /* Configure TCP Options */
+    Config::SetDefault("ns3::TcpSocket::SegmentSize", UintegerValue(payloadSize));
+
+    NodeContainer p2pNodes;
+    p2pNodes.Create(2);
+
+    PointToPointHelper pointToPoint;
+    pointToPoint.SetDeviceAttribute("DataRate", StringValue("100Mbps"));
+    pointToPoint.SetChannelAttribute("Delay", StringValue("10ms"));
+    pointToPoint.SetQueue("ns3::DropTailQueue");
+
+    NetDeviceContainer p2pDevices;
+    p2pDevices = pointToPoint.Install(p2pNodes);
+
+    NodeContainer csmaNodes;
+    csmaNodes.Add(p2pNodes.Get(1));
+    csmaNodes.Create(nCsma);
+
+    CsmaHelper csma;
+    csma.SetChannelAttribute("DataRate", StringValue("100Mbps"));
+    csma.SetChannelAttribute("Delay", TimeValue(NanoSeconds(6560)));
+
+    NetDeviceContainer csmaDevices;
+    csmaDevices = csma.Install(csmaNodes);
+
+    /**
+     * @brief from here
+     */
+    NodeContainer staWifiNodes;
+    staWifiNodes.Create(nWifi);
+
+    NodeContainer apWifiNode = p2pNodes.Get(0);
+
+    YansWifiChannelHelper wifiChannel = YansWifiChannelHelper::Default();
+    // wifiChannel.SetPropagationDelay("ns3::ConstantSpeedPropagationDelayModel"); // TODO delete
+    // wifiChannel.AddPropagationLoss("ns3::FrissPropagationLossModel", "Frequency", DoubleValue(5e9)); // TODO delete
+
+    YansWifiPhyHelper wifiPhy;
+    wifiPhy.SetChannel(wifiChannel.Create());
+    wifiPhy.SetPcapDataLinkType(WifiPhyHelper::DLT_IEEE802_11_RADIO);
+    // wifiPhy.SetErrorRateModel("ns3::YansErrorRateModel"); // TODO delete
+
+    WifiHelper wifiHelper;
+    wifiHelper.SetRemoteStationManager("ns3::AarfWifiManager");
+    // wifiHelper.SetStandard ("WIFI_STANDARD_80211n_5GHZ"); //TODO delete
+    // wifiHelper.SetRemoteStationManager("ns3::ConstantRateWifiManager",
+    //                                    "DataMode", StringValue(phyRate),
+    //                                    "ControlMode", StringValue("HtMcs0")); // TODO delete
+
+    WifiMacHelper wifiMac;
+
+    /* Configure STA */
+    Ssid ssid = Ssid("network");
+    wifiMac.SetType("ns3::StaWifiMac",
+                    "Ssid", SsidValue(ssid), "ActiveProbing", BooleanValue(false)); // TODO check
+
+    NetDeviceContainer staDevices;
+    staDevices = wifiHelper.Install(wifiPhy, wifiMac, staWifiNodes);
+
+    /* Configure AP */
+    wifiMac.SetType("ns3::ApWifiMac",
+                    "Ssid", SsidValue(ssid));
+
+    NetDeviceContainer apDevices;
+    apDevices = wifiHelper.Install(wifiPhy, wifiMac, apWifiNode);
+
+    /*  Mobility Model */
+    MobilityHelper mobility;
+
+    mobility.SetPositionAllocator("ns3::GridPositionAllocator",
+                                  "MinX", DoubleValue(0.0),
+                                  "MinY", DoubleValue(0.0),
+                                  "DeltaX", DoubleValue(5.0),
+                                  "DeltaY", DoubleValue(10.0),
+                                  "GridWidth", UintegerValue(3),
+                                  "LayoutType", StringValue("RowFirst"));
+
+    mobility.SetMobilityModel("ns3::RandomWalk2dMobilityModel",
+                              "Bounds", RectangleValue(Rectangle(-50, 50, -50, 50)));
+    mobility.Install(staWifiNodes);
+    mobility.Install(apWifiNode);
+
+    // TODO delete
+    // mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+    // mobility.Install(apWifiNode);
+    // mobility.Install(staWifiNodes);
+
+    /* Internet Stack */
+    InternetStackHelper stack;
+    // stack.Install(p2pNodes);
+    stack.Install(csmaNodes); // TODO check
+    stack.Install(apWifiNode);
+    stack.Install(staWifiNodes);
+
+    Ipv4AddressHelper address;
+    address.SetBase("10.1.1.0", "255.255.255.0");
+
+    Ipv4InterfaceContainer p2pInterfaces;
+    p2pInterfaces = address.Assign(p2pDevices);
+    address.SetBase("10.1.2.0", "255.255.255.0");
+
+    Ipv4InterfaceContainer csmaInterfaces;
+    csmaInterfaces = address.Assign(csmaDevices);
+    address.SetBase("10.1.3.0", "255.255.255.0");
+
+    Ipv4InterfaceContainer apInterface;
+    apInterface = address.Assign(apDevices);
+    Ipv4InterfaceContainer staInterface;
+    staInterface = address.Assign(staDevices);
+
+    /* Populate routing table */
+    Ipv4GlobalRoutingHelper::PopulateRoutingTables();
+
+    /* Install TCP Receiver on the access point */
+    Ptr<RateErrorModel> em = CreateObject<RateErrorModel>();
+    em->SetAttribute("ErrorRate", DoubleValue(0.00001));
+    for (int i = 0; i < no_of_flow; i++)
     {
-        LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
-        LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
+        csmaDevices.Get(i + 1)->SetAttribute("ReceiveErrorModel", PointerValue(em));
+
+        PacketSinkHelper sinkHelper("ns3::TcpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), 9 + i));
+        ApplicationContainer sinkApp = sinkHelper.Install(staWifiNodes.Get(i));
+        sink = StaticCast<PacketSink>(sinkApp.Get(0));
+
+        /* Install TCP/UDP Transmitter on the station */
+        OnOffHelper server("ns3::TcpSocketFactory", (InetSocketAddress(staInterface.GetAddress(i), 9 + i)));
+        server.SetAttribute("PacketSize", UintegerValue(payloadSize));
+        server.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=1]"));
+        server.SetAttribute("OffTime", StringValue("ns3::ConstantRandomVariable[Constant=0]"));
+        server.SetAttribute("DataRate", DataRateValue(DataRate(dataRate)));
+        ApplicationContainer serverApp = server.Install(csmaNodes.Get(i + 1)); // server node assign
+
+        /* Start Applications */
+        sinkApp.Start(Seconds(0.0));
+        serverApp.Start(Seconds(1.0));
     }
 
+    Simulator::Schedule(Seconds(1.1), &CalculateThroughput);
 
+    /* Flow Monitor */
+    Ptr<FlowMonitor> flowMonitor;
+    FlowMonitorHelper flowHelper;
+    flowMonitor = flowHelper.InstallAll();
+
+    flowMonitor->CheckForLostPackets();
+    Ptr<Ipv4FlowClassifier> classifier = DynamicCast<Ipv4FlowClassifier>(flowHelper.GetClassifier());
+    std::map<FlowId, FlowMonitor::FlowStats> stats = flowMonitor->GetFlowStats();
+
+    /* Start Simulation */
+    Simulator::Stop(Seconds(simulationTime + 1));
+    Simulator::Run();
+
+    /* Flow Monitor File  */
+    flowMonitor->SerializeToXmlFile("./highrateSimulation/history.flowmonitor", false, false);
+
+    double averageThroughput = ((sink->GetTotalRx() * 8) / (1e6 * simulationTime));
+
+    Simulator::Destroy();
+
+    if (averageThroughput < 50)
+    {
+        NS_LOG_ERROR("Obtained throughput is not in the expected boundaries!");
+        exit(1);
+    }
+    std::cout << "\nAverage throughput: " << averageThroughput << " Mbit/s" << std::endl;
+    return 0;
 }
